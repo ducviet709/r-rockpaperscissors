191e7ab7eeb69b58b2755cf643cc4776ce638021
viet duc <ducviet709@gmail.com>
2020-07-22 10:11:00 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,91 @@
-import React from 'react';
-
+import React, { useState } from "react";
 import './App.css';
 import ChoiceCard from "./components/ChoiceCard";
 
-const choices = {
-  rock:
-    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
-  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
-  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+
+export const getRandomChoice = () => {
+  let choiceNames = Object.keys(CHOICES); 
+  let randomIndex = Math.floor(Math.random() * 3); 
+  let choiceName = choiceNames[randomIndex];
+  return CHOICES[choiceName];
+};
+export const getRoundOutcome = userChoice => {
+  const computerChoice = getRandomChoice().name;
+  let result;
+
+  if (userChoice === "rock") {
+    result = computerChoice === "scissors" ? "Victory!" : "Defeat!";
+  }
+  if (userChoice === "paper") {
+    result = computerChoice === "rock" ? "Victory!" : "Defeat!";
+  }
+  if (userChoice === "scissors") {
+    result = computerChoice === "paper" ? "Victory!" : "Defeat!";
+  }
+
+  if (userChoice === computerChoice) result = "Tie game!";
+  return [result, computerChoice];
 };
 
+export const CHOICES = {
+  scissors: {
+    name: "scissors",
+    url: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+  },
+  paper: {
+    name: "paper",
+    url: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png"
+  },
+  rock: {
+    name: "rock",
+    url:
+      "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png"
+  }
+};
 function App() {
+  const [gameHistory, setGameHistory] = useState([]);
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [previousWinner, setPreviousWinner] = useState(null);
+  
+
+  const onPlayerChoose = playerChoice => {
+    const [result, compChoice] = getRoundOutcome(playerChoice);
+
+    const newUserChoice = CHOICES[playerChoice];
+    const newComputerChoice = CHOICES[compChoice];
+
+    setPlayerChoice(newUserChoice);
+    setComputerChoice(newComputerChoice);
+
+    if (result === "Victory!") {
+      setPreviousWinner("You");
+    } else if (result === "Defeat!") {
+      setPreviousWinner("Computer");
+    } else {
+      setPreviousWinner("Tie");
+    }
+
+ 
+    gameHistory.push(result);
+    setGameHistory(gameHistory);
+  };
+  
   return (
     <div className="App">
-      <ChoiceCard title="You" winner={false} imgURL={choices.rock}/>
-      <ChoiceCard title="Computer" winner={true} imgURL={choices.paper}/>
+      <ChoiceCard title="You"  imgURL={playerChoice && playerChoice.url}  previousWinner={previousWinner} />
+      <div className="container">
+        <button className="btn btn-success btn-lg" onClick={() => onPlayerChoose("rock")}>Rock</button>
+        <button className="btn btn-success btn-lg" onClick={() => onPlayerChoose("paper")}>Paper</button>
+        <button className="btn btn-success btn-lg" onClick={() => onPlayerChoose("scissors")}>Scissors</button>
+        <h3>History</h3>
+  <ul>
+    {gameHistory.map(result => {
+      return <li>{result}</li>;
+    })}
+  </ul>
+      </div>
+      <ChoiceCard title="Computer"  imgURL={computerChoice && computerChoice.url}  previousWinner={previousWinner} />
     </div>
   );
 }
